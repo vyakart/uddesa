5027408fd63b1a4e2bc66110212214b3c38eb8f8
Vyakart <nishit@tuta.io>
2026-02-27 00:22:41 +0530

    perf: keep excalidraw css off shelf critical path

--- muwi/src/components/diaries/blackboard/index.ts
+++ muwi/src/components/diaries/blackboard/index.ts
@@ -1,5 +1,4 @@
 // Blackboard components exports
 export { Blackboard } from './Blackboard';
-export { ExcalidrawWrapper } from './ExcalidrawWrapper';
 export { IndexPanel } from './IndexPanel';
 export { BlackboardToolbar } from './BlackboardToolbar';
